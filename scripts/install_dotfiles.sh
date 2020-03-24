43b5d8eb080a759e0e8f193920dec39926a7d5ce
Axel Weber <weberal@studi.informatik.uni-stuttgart.de>
2020-03-24 20:55:22 +0100

    fixed typos

--- scripts/install_dotfiles.sh
+++ scripts/install_dotfiles.sh
@@ -3,15 +3,15 @@
 
 git clone --bare https://github.com/weberax/dotfiles.git $HOME/.dotconf
 
-function dotgit { git --git-dir=$HOME/.dotconf --work-tree=$HOME $@ }
+function dotgit { git --git-dir=$HOME/.dotconf --work-tree=$HOME $@ ; }
 
 #this doesn't work for dir's so far
-dotgit checkout || mkdir -p .conf.old && \
-    dotgit checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .conf.old/{} && \
-    echo "moved existing files to .conf.old/"
+dotgit checkout || mkdir -p .old_configs && \
+    dotgit checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .old_configs/{} && \
+    echo "moved existing files to .old_configs/"
 
 dotgit checkout
 dotgit submodule init
 dotgit submodule update
-dotgit confit status.showUntrackedFiles no
-exit(0)
+dotgit config status.showUntrackedFiles no
+exit 0
